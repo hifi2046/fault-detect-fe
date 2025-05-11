74d194fef9e46bb5430644c9f68494643bf0568a
huangyinfei <huangyinfei@fusionfintrade.com>
2025-05-11 11:23:16 +0800

    table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,16 @@
 <!--Home.vue-->
 <template>
-    <div>
-      Home
-    </div>
-  </template>
-  
+  <el-table :data="data.info">
+    <el-table-column prop="id" label="编号" width="100"/>
+    <el-table-column prop="account" label="账号" width="300"/>
+    <el-table-column prop="side" label="业务" width="100"/>
+    <el-table-column prop="amount" label="金额" width="500"/>
+    <el-table-column prop="rule" label="触发规则" width="100"/>
+    <el-table-column prop="content" label="报警内容" width="3000"/>
+  </el-table>
+</template>
+<script setup>
+  import {ref,reactive} from 'vue'
+  import axios from 'axios'
+  const data=reactive({info: []});
+</script> 
