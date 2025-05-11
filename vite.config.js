e4f889e75af9b0d2db7417fab582b4f3a08fc518
huangyinfei <huangyinfei@fusionfintrade.com>
2025-05-11 10:08:58 +0800

    port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+    port: 8000,
+    open: false,
+    cors: true,
+    strictPort: false,
+  }
 })
